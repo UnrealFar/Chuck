f2b545c8e25b0821e3f5b3b36dee51bcfc0c10a7
Far.gg <73272708+TheFarGG@users.noreply.github.com>
2021-09-15 07:46:56 +0000

    fixes

--- bot.py
+++ bot.py
@@ -27,9 +27,9 @@ async def on_guild_join(guild):
         channel = guild.system_channel
         if channel is not None:
             joinEm=discord.Embed(title="Thank you for adding me to the server!", description="", colour=discord.Colour.blurple())
-            joinEm.add_field(name="Prefix", value="The bot's prefix is ?", inline=False)
+            joinEm.add_field(name="Prefix", value="The bot's prefix is c!", inline=False)
             joinEm.add_field(name="Commands", value="Do ?help for a list of all the bot commands!", inline=False)
-            joinEm.set_footer(icon_url="https://images-ext-1.discordapp.net/external/2NTlKKViwT-O75ct7SeCkG_hSa5SsBYvw9csO1JEfwo/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/859996173943177226/19ec5346838e19dab0d715d5820f2b5a.webp", text="With love, PikaPi#6298")
+            joinEm.set_footer(icon_url="https://images-ext-1.discordapp.net/external/1qUCiiAC2VexuiXFtwtRUjhY-7YO5xUzHeiRS6rJ7QA/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/859996173943177226/cb3a07af653f2878ba1795256dcb822c.png?width=598&height=598", text="With love, Far.GG#6298")
         await channel.send(embed=joinEm)
     except:
         pass
@@ -100,7 +100,7 @@ class HelpCommand(commands.HelpCommand):
         await self.get_destination().send(embed=embed)
 
     async def send_cog_help(self, cog_):
-        embed = discord.Embed(title="{0.qualified_name} Commands".format(cog_))
+        embed = discord.Embed(title=f"{cog_.qualified_name} Commands")
         if cog_.description:
             embed.description = cog_.description
 
@@ -137,11 +137,12 @@ class HelpCommand(commands.HelpCommand):
 
 bot.help_command = HelpCommand()
 
-initial_extensions = ['cogs.misc','cogs.mod','cogs.fun','cogs.events','cogs.economy','cogs.owner','cogs.nsfw','cogs.giveaways']
+initial_extensions = ['cogs.misc','cogs.mod','cogs.fun','cogs.events','cogs.economy','cogs.owner','cogs.nsfw','cogs.giveaways', 'cogs.levels']
 if __name__ == '__main__':
     for extension in initial_extensions:
         bot.load_extension(extension)
 
+@bot.message_command()
 @bot.slash_command(guild_ids = [860767299236921344])
 async def load(ctx, *, name: str):
     if ctx.author.id != 859996173943177226:
@@ -149,40 +150,44 @@ async def load(ctx, *, name: str):
         return
     try:
         bot.load_extension(f"cogs.{name}")
+        await ctx.send(f'"**{name}**" Cog loaded!')
     except Exception as e:
         await ctx.send(e)
-    await ctx.send(f'"**{name}**" Cog loaded!')
 
+@bot.message_command()
 @bot.slash_command(guild_ids = [860767299236921344])
 async def reload(ctx, *, name: str):
     if ctx.author.id != 859996173943177226:
-        await ctx.respond("Only my owner can use this command ;-;")
+        await ctx.send("Only my owner can use this command ;-;")
         return
     try:
         bot.reload_extension(f"cogs.{name}")
+        await ctx.send(f'"**{name}**" Cog reloaded!')
     except Exception as e:
         await ctx.send(e)
-    await ctx.respond(f'"**{name}**" Cog reloaded!')
 
+@bot.message_command()
 @bot.slash_command(guild_ids = [860767299236921344])
 async def unload(ctx, *, name: str):
     if ctx.author.id != 859996173943177226:
-        await ctx.respond("Only my owner can use this command ;-;")
+        await ctx.send("Only my owner can use this command ;-;")
     try:
         bot.unload_extension(f"cogs.{name}")
+        await ctx.send(f'"**{name}**" Cog unloaded!')
     except Exception as e:
         await ctx.send(e)
-    await ctx.send(f'"**{name}**" Cog unloaded!')
 
+@bot.message_command()
 @bot.slash_command()
 async def ping(ctx):
     """🏓Shows Chuck's ping"""
     bot_ping = round(bot.latency * 1000)
     pingEm = discord.Embed(title="Pong!", description="", colour = discord.Color.blurple())
     pingEm.add_field(name="Bot Ping", value=f"🏓 {bot_ping}ms", inline=False)
-    await ctx.respond(embed = pingEm)
+    await ctx.send(embed = pingEm)
 
-@bot.slash_command(guild_ids = [860767299236921344, 885465324206579712])
+@bot.message_command()
+@bot.slash_command()
 async def unbanall(ctx):
     if ctx.author.id != 859996173943177226:
         await ctx.respond("Only my owner can use this command ;-;")
@@ -191,8 +196,8 @@ async def unbanall(ctx):
     try:
         for ban_entry in banned_users:
             await ctx.guild.unban(ban_entry.user)
-        await ctx.respond(f"Unbaned everyone!")
+        await ctx.send(f"Unbaned everyone!")
     except Exception as e:
-        await ctx.respond(f"{e}")
+        await ctx.send(f"{e}")
 
 bot.run(TOKEN)
